277194362e38975f145e119af0558ff634f4d279
Tell <55622895+Tell396@users.noreply.github.com>
2021-12-17 19:19:17 +1000

    Added main files

---
+++ main.js
@@ -0,0 +1,104 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio)
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(5);
+
+// renderer.render(scene, camera);
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry( 10, 3, 16, 100),
+  new THREE.MeshStandardMaterial( { color: 0x8860d0, wireframe: false } )
+);
+scene.add( torus );
+
+const pointLight = new THREE.PointLight( 0xff0000);
+pointLight.position.set( 20, 20, 20 );
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add( pointLight, ambientLight );
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add( lightHelper, gridHelper ); // Delete a gridHelper
+
+// Avatar
+
+const avatarTexture = new THREE.TextureLoader().load('favicon.svg')
+
+const avatar = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial( { map: avatarTexture })
+); 
+
+scene.add(avatar)
+
+// Moon
+const moonTexture = new THREE.TextureLoader().load('img/moon.jpg')
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshBasicMaterial( { map: moonTexture })
+);
+
+scene.add(moon)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.24, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(13).fill().map(() => THREE.MathUtils.randFloatSpread( 120 ));
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+Array(200).fill().forEach(addStar);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+avatar.position.z = -5;
+avatar.position.x = 2;
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  avatar.rotation.y += 0.01;
+  avatar.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+
+const spaceTexture = new THREE.TextureLoader().load('img/nebula.jpg');
+scene.background = spaceTexture;
+
+function animate() {
+  requestAnimationFrame( animate )
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.007;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render( scene, camera );
+}
+
+animate()
